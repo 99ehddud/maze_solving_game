13ad725f0771b9a0ede6c14761afb15ac7ed509c
99ehddud <99ehddud@gmail.com>
2022-05-05 00:45:13 +0900

    Ver.x.1.2 (Modify Outline)

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-function resize() {
+function adjust_size() {
     const header = document.querySelector('header');
     const main = document.querySelector('main');
+    const playbox = document.querySelector('#playbox');
     const footer = document.querySelector('footer');
 
+    const x_tap_status_height = screen.height - (window.outerHeight - window.innerHeight);
 
-    header.style.width = screen.availWidth + 'px';
-
-    main.style.width = screen.availWidth + 'px';
-
-    footer.style.width = screen.availWidth + 'px';
+    header.style.width = screen.width + 'px';
+    header.style.height = (x_tap_status_height * 0.1) + 'px';
+    main.style.width = screen.width + 'px';
+    main.style.height = (x_tap_status_height * 0.85) + 'px';
+    playbox.style.width = (screen.width * 0.6) + 'px';
+    playbox.style.height = (x_tap_status_height * 0.85) + 'px';
+    footer.style.width = screen.width + 'px';
+    footer.style.height = (x_tap_status_height * 0.05) + 'px';
 }
